0e39f2c9f96389fe16d55499adea07ea02421212
IT-Dasha <144232047+IT-Dasha@users.noreply.github.com>
2025-02-02 22:56:54 +0300

    Програ готова)

--- To-Do List/To-Do List/MainWindow.xaml.cs
+++ To-Do List/To-Do List/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace To_Do_List
         public MainWindow()
         {
             InitializeComponent();
+            this.Icon = new BitmapImage(new Uri("icon.ico", UriKind.RelativeOrAbsolute));
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -44,26 +45,30 @@ namespace To_Do_List
                 // Создаем новый текстовый элемент
                 TextBlock newTextBlock = new TextBlock();
 
-                // Устанавливаем текст
+                // Устанавливаем  
                 newTextBlock.Text = newItem;
 
                 // Устанавливаем цвет текста в зависимости от выбранного элемента
                 if (list_priority.SelectedIndex == 0)
                 {
-                    newTextBlock.Foreground = new SolidColorBrush(Colors.Red); 
+                    newTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                    newTextBlock.Background = new SolidColorBrush(Colors.Red);
                 }
                 else if (list_priority.SelectedIndex == 1)
                 {
-                    newTextBlock.Foreground = new SolidColorBrush(Colors.Orange);
+                    newTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+                    newTextBlock.Background = new SolidColorBrush(Colors.Orange);
                 }
                 else if (list_priority.SelectedIndex == 2)
                 {
-                    newTextBlock.Foreground = new SolidColorBrush(Colors.Yellow); 
+                    newTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+                    newTextBlock.Background = new SolidColorBrush(Colors.Yellow);
                 }
 
                 else if (list_priority.SelectedIndex == 3)
                 {
-                    newTextBlock.Foreground = new SolidColorBrush(Colors.Green); 
+                    newTextBlock.Foreground = new SolidColorBrush(Colors.White);
+                    newTextBlock.Background = new SolidColorBrush(Colors.Green);
                 }
 
                 // Добавляем новый элемент в ListBox
